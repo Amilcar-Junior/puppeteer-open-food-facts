2c6fcd22bc37a375987e783a2a800b0f2f28c3a0
amilcar.junior <amilcar.junior@health.cv>
2024-02-22 22:38:17 -0100

    otimização

--- src/index.js
+++ src/index.js
@@ -7,16 +7,14 @@ const fs = require("fs");
     defaultViewport: false,
   });
   const page = await browser.newPage();
-  await page.goto("https://br.openfoodfacts.org", {
-    waitUntil: "load",
-  });
+  await page.goto("https://br.openfoodfacts.org");
   let products = []; // Array para armazenar os dados dos produtos
   let productsDetails = []; // Array para armazenar os dados dos produtos
 
-  let isBtnDisabled = false;
-  while (!isBtnDisabled) {
-    await new Promise((resolve) => setTimeout(resolve, 1000)); // Espera 1 segundo antes de continuar
+  let hasNextPage = true;
+  while (hasNextPage) {
     //guarda todos os produtos da pagina principal
+    await page.waitForSelector("#products_match_all");
     const productsHandles = await page.$$(".search_results > li");
 
     for (let i = 0; i < productsHandles.length; i++) {
@@ -24,58 +22,44 @@ const fs = require("fs");
         const productsHandles = await page.$$(".search_results > li"); // Re-seleciona os elementos após cada navegação
         //clica no produto[i]
         await productsHandles[i].click();
-        await page.waitForNavigation();
+        //esperar carregar esses 3 seletores que vamos coletar dados, otimizando sem precisar esperar dados desnecessários
+        await Promise.all([
+          page.waitForSelector("#product", { timeout: 5000 }).catch(() => {}),
+          page.waitForSelector("#match", { timeout: 5000 }).catch(() => {}),
+          page.waitForSelector("#health", { timeout: 5000 }).catch(() => {}),
+        ]);
 
         // Dentro do Produto
-        // variaveis dos selectors
+
+        //coleta o id do produto
         const idElement = await page.$("#barcode");
-        const titleElementSelector1 =
-          "#product > div > div > div.card-section > h2";
-        const titleElementSelector2 =
-          "#product > div > div > div.card-section > div > div.medium-8.small-12.columns > h2";
-
-        const quantityElement = await page.$("#field_quantity_value");
-
-        const ingredientsListElement = await page.$(
-          "#ordered_ingredients_list"
-        );
-        const nutritionScoreElement = await page.$(
-          "#attributes_grid > li:nth-child(1) > a > div > div > div.attr_text > h4"
-        );
-        const nutritionTitleElement = await page.$(
-          "#attributes_grid > li:nth-child(1) > a > div > div > div.attr_text > span"
-        );
-        const novaScoreElement = await page.$(
-          "#attributes_grid > li:nth-child(2) > a > div > div > div.attr_text > h4"
-        );
-        const novaTitleElement = await page.$(
-          "#attributes_grid > li:nth-child(2) > a > div > div > div.attr_text > span"
-        );
-        const servingSizeElement = await page.$(
-          "#panel_serving_size_content > div > div > div"
-        );
-
         const id = idElement
-          ? await page.evaluate((element) => element.textContent, idElement)
-          : "null";
+          ? await page.evaluate(
+              (element) => element.textContent.trim(),
+              idElement
+            )
+          : null;
+        console.log("Id:", id);
 
-        let titleElement = await page.$(titleElementSelector1);
-        if (!titleElement) {
-          titleElement = await page.$(titleElementSelector2);
-        }
+        //coleta o nome do produto
+        const titleElement = await page.$(".title-1");
         const title = titleElement
           ? await page.evaluate(
               (element) => element.textContent.trim(),
               titleElement
             )
-          : "null";
+          : null;
+        console.log("Nome:", title);
 
+        //coleta quantidade do produto
+        const quantityElement = await page.$("#field_quantity_value");
         const quantity = quantityElement
           ? await page.evaluate(
-              (element) => element.textContent,
+              (element) => element.textContent.trim(),
               quantityElement
             )
-          : "null";
+          : null;
+        console.log("Quantidade:", quantity);
 
         //4 possiveis ids que pode se obter o valor
         const palmOilStatusMap = {
@@ -85,7 +69,7 @@ const fs = require("fs");
           "#panel_ingredients_analysis_en-palm-oil": true,
         };
 
-        let hasPalmOil = "unknown"; // Valor padrão
+        let hasPalmOil = "unknown";
         //verifica de algum dos 4 id esta foi encontrado e seta o valor
         for (const id in palmOilStatusMap) {
           if (await page.$(id)) {
@@ -102,7 +86,7 @@ const fs = require("fs");
           "#panel_ingredients_analysis_en-vegan": true,
         };
 
-        let isVegan = "unknown"; // Valor padrão
+        let isVegan = "unknown";
         //verifica de algum dos 4 id esta foi encontrado e seta o valor
         for (const id in veganStatusMap) {
           if (await page.$(id)) {
@@ -119,7 +103,7 @@ const fs = require("fs");
           "#panel_ingredients_analysis_en-vegetarian": true,
         };
 
-        let isVegetarian = "unknown"; // Valor padrão
+        let isVegetarian = "unknown";
         //verifica de algum dos 4 id esta foi encontrado e seta o valor
         for (const id in vegetarianStatusMap) {
           if (await page.$(id)) {
@@ -128,14 +112,21 @@ const fs = require("fs");
           }
         }
 
+        //coleta o Title da nutrição
+        const nutritionTitleElement = await page.$(
+          "#attributes_grid > li:nth-child(1) > a > div > div > div.attr_text > span"
+        );
         const nutritionTitle = nutritionTitleElement
           ? await page.evaluate(
               (element) => element.textContent,
               nutritionTitleElement
             )
-          : "null";
+          : null;
 
         //coleta todos os dados span dentros do li e junta com ,
+        const ingredientsListElement = await page.$(
+          "#ordered_ingredients_list"
+        );
         const ingredientsList = ingredientsListElement
           ? await page.evaluate((element) => {
               const listItems = element.querySelectorAll("li");
@@ -144,16 +135,19 @@ const fs = require("fs");
               );
               return [ingredients.join(", ")];
             }, ingredientsListElement)
-          : ["null"];
+          : [null];
 
         //coleta o valor do nutri-score e remove o texto de nutri-score para obter apenas (A,B,C,D,E,F)
+        const nutritionScoreElement = await page.$(
+          "#attributes_grid > li:nth-child(1) > a > div > div > div.attr_text > h4"
+        );
         const nutritionScore = nutritionScoreElement
           ? await page.evaluate((element) => {
               const fullText = element.textContent;
               const score = fullText.replace("Nutri-Score ", "");
               return score.trim();
             }, nutritionScoreElement)
-          : "null";
+          : null;
 
         //verifica atravez as imagem para definir level e pega o texto
         const nutrientLevels = await page.evaluate(() => {
@@ -181,6 +175,9 @@ const fs = require("fs");
         // console.log(nutrientLevels);
 
         //coleta os dados de tamanho de porção e remove o texto Tamanho da porção:
+        const servingSizeElement = await page.$(
+          "#panel_serving_size_content > div > div > div"
+        );
         const servingSize = servingSizeElement
           ? (
               await page.evaluate(
@@ -190,74 +187,68 @@ const fs = require("fs");
             )
               .replace("Tamanho da porção:", "")
               .trim()
-          : "null";
+          : null;
 
-        const nutritionFactsTable = await page.$(
+        //percore o tbody da tabela e coleta dados da tabela
+        const nutritionDataElement = await page.$(
           "#panel_nutrition_facts_table_content > div > table > tbody"
         );
-        let nutritionData = {};
-        if (nutritionFactsTable) {
-          const rows = await nutritionFactsTable.$$("tr");
+        const nutritionData = nutritionDataElement
+          ? await page.evaluate((tableBody) => {
+              const rows = Array.from(tableBody.querySelectorAll("tr"));
 
-          if (rows.length > 0) {
-            // Extrai os dados de cada linha da tabela
-            for (let j = 0; j < rows.length - 1; j++) {
-              const tds = await rows[j].$$("td");
-              //pega ex: energia
-              if (tds.length > 0) {
-                const columnNameSpan = await tds[0].$("span");
-                const columnName = await page.evaluate(
-                  (element) => element.textContent.trim(),
-                  columnNameSpan
-                );
+              return (
+                rows
+                  // .slice(0, -1)
+                  .map((row) => {
+                    const columns = Array.from(row.querySelectorAll("td"));
+                    const columnName = columns[0]
+                      .querySelector("span")
+                      .textContent.trim();
+                    const columnValuePer100g = columns[1]
+                      ? columns[1].querySelector("span").textContent.trim()
+                      : null;
+                    const columnValuePerServing = columns[2]
+                      ? columns[2].querySelector("span").textContent.trim()
+                      : null;
 
-                let columnValuePer100g = null;
-                let columnValuePerServing = null;
-                //pega ex: per100 para energia
-                if (tds.length > 1) {
-                  const columnValueSpanPer100g = await tds[1].$("span");
-                  columnValuePer100g = await page.evaluate(
-                    (element) => element.textContent.trim(),
-                    columnValueSpanPer100g
-                  );
-                  //pega ex: PerServing para energia
-                  if (tds.length > 2) {
-                    const columnValuePerServingSpan = await tds[2].$("span");
-                    columnValuePerServing = await page.evaluate(
-                      (element) => element.textContent.trim(),
-                      columnValuePerServingSpan
-                    );
-                  }
-                }
-                // Remove as aspas do nome da coluna, se houver
-                const cleanColumnName = columnName.replace(/['"]/g, "");
-
-                nutritionData[cleanColumnName] = {
-                  per100g: columnValuePer100g,
-                  perServing: columnValuePerServing,
-                };
-              }
-            }
-            // console.log(nutritionData);
-          }
-        }
+                    return {
+                      [columnName]: {
+                        per100g: columnValuePer100g,
+                        perServing: columnValuePerServing,
+                      },
+                    };
+                  })
+                  .reduce((acc, cur) => {
+                    return { ...acc, ...cur };
+                  }, {})
+              );
+            }, nutritionDataElement)
+          : null;
+        console.log("data:", nutritionData);
 
         //pega o valor de novascore e remove o texto NOVA
+        const novaScoreElement = await page.$(
+          "#attributes_grid > li:nth-child(2) > a > div > div > div.attr_text > h4"
+        );
         const novaScore = novaScoreElement
           ? await page.evaluate((element) => {
               const fullText = element.textContent;
               const score = parseInt(fullText.replace("NOVA ", ""));
               return score;
             }, novaScoreElement)
-          : "null";
+          : null;
 
         //pega o valor de novaTitle
+        const novaTitleElement = await page.$(
+          "#attributes_grid > li:nth-child(2) > a > div > div > div.attr_text > span"
+        );
         const novaTitle = novaTitleElement
           ? await page.evaluate(
               (element) => element.textContent,
               novaTitleElement
             )
-          : "null";
+          : null;
 
         //fortatação de como os produtos vão ficar salvos no products.json
         const product = {
@@ -302,42 +293,37 @@ const fs = require("fs");
         console.log(product);
         console.log(productDetails);
 
-
-        // Descomentar caso queira salvar os dados no json em tempo real 
+        // Descomentar caso queira salvar os dados no json em tempo real
         // PS: Na pratica faz com que o projeto fique mais lento pois tem que salvar o arquivo a cada produto
         // Salva os dados em um arquivo JSON a cada item encontrado
 
-        // fs.writeFileSync("./data/products.json", JSON.stringify(products, null, 2));
+        // fs.writeFileSync(
+        //   "./data/products.json",
+        //   JSON.stringify(products, null, 2)
+        // );
         // fs.writeFileSync(
         //   "./data/productsDetails.json",
         //   JSON.stringify(productsDetails, null, 2)
         // );
 
+        //sai do produto
         await page.goBack();
       } catch (error) {
         console.error(error);
       }
     }
 
-    //pego os valores do penultimo li de paginação onde fica o botão proximo
-    //caso o penultimo tiver classe current quer dizer que o proximo desapareceu e ficou a ultima pagina no penultimo li
-    const isDisabled = await page.evaluate(() => {
-      const penultimatePage = document.querySelector(
-        ".pagination > li:nth-last-child(2)"
+    try {
+      await page.waitForSelector("#pages", { timeout: 5000 }).catch(() => {});
+      const nextPageUrl = await page.$eval(
+        '#pages li a[rel="next$nofollow"]',
+        (el) => el.href
       );
-      return (
-        penultimatePage !== null &&
-        penultimatePage.classList.contains("current") 
-      );
-    });
-    //clica no botão proximo
-    if (!isDisabled) {
-      try {
-        await page.click(".pagination > li:nth-last-child(2)");
-        await new Promise((resolve) => setTimeout(resolve, 1000));
-      } catch (error) {
-        console.error(error);
-      }
+      // Navega para a próxima página e espera até que a página esteja pronta
+      await page.goto(nextPageUrl, { waitUntil: "networkidle2" });
+    } catch (error) {
+      console.log("Não tem mais paginas");
+      hasNextPage = false;
     }
   }
 
@@ -347,5 +333,6 @@ const fs = require("fs");
     "./data/productsDetails.json",
     JSON.stringify(productsDetails, null, 2)
   );
+  console.log("Todos os dados Coletados")
   await browser.close();
 })();
